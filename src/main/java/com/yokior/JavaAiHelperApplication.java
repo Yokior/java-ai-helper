a5eb8d6df7f0af11ea4f2d40f83a1c360b1a46b4
Yokior <13814108217@163.com>
2026-01-04 21:01:04 +0800

    init: 初始化项目

---
+++ src/main/java/com/yokior/JavaAiHelperApplication.java
@@ -0,0 +1,13 @@
+package com.yokior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAiHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAiHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yokior/JavaAiHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yokior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAiHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
