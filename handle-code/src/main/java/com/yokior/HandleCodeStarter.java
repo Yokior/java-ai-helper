221f3f1878cf5a3dda5e8ee56e293e3fa019b356
Yokior <13814108217@163.com>
2026-01-04 21:45:46 +0800

    init: 初始化搭建

--- handle-code/src/main/java/com/yokior/HandleCodeStarter.java
+++ handle-code/src/main/java/com/yokior/HandleCodeStarter.java
@@ -3,11 +3,14 @@ package com.yokior;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Yokior
+ * @description
+ * @date 2026/1/4 21:35
+ */
 @SpringBootApplication
-public class JavaAiHelperApplication {
-
+public class HandleCodeStarter {
     public static void main(String[] args) {
-        SpringApplication.run(JavaAiHelperApplication.class, args);
+        SpringApplication.run(HandleCodeStarter.class, args);
     }
-
 }
---
+++ handle-code/src/test/java/com/yokior/LoadFileTest.java
@@ -0,0 +1,15 @@
+package com.yokior;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Yokior
+ * @description
+ * @date 2026/1/4 21:44
+ */
+
+public class LoadFileTest {
+    
+
+}
+
